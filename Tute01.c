652105366528137953466c842fb206eec2ffa8d6
Sri Samadhi L.A.S.S <100861562+it21302862@users.noreply.github.com>
2022-03-03 17:28:55 +0000

    tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,21 @@
 
 int main() {
   
-  return 0;
-}
+   int mark1,mark2,sum; //get input
+   float average;
+  
+   printf("enter marks 1 : ");//ask sub marks
+   scanf("%d",&mark1);
+  
+   printf("enter marks 2 : ");//ask sub marks
+   scanf("%d",&mark2);
+   
+   sum=mark1+mark2;
+   
+   average=sum/2.0;
+
+   printf("average of the marks: %.2f ",average);
+
+   return 0;
+  }
 
